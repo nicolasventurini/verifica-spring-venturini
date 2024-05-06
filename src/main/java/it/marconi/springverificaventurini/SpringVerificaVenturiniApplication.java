c7227e6cd11be322428346e80c382cd29f9ccc17
nicolasventurini <nicolas.venturini@marconirovereto.it>
2024-05-06 08:13:42 +0200

    Creazione repository per verifica

---
+++ src/main/java/it/marconi/springverificaventurini/SpringVerificaVenturiniApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.springverificaventurini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVerificaVenturiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVerificaVenturiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/springverificaventurini/SpringVerificaVenturiniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.springverificaventurini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVerificaVenturiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
